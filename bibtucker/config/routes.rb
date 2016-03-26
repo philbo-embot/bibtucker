097f5b05ec2309b8f58d63b060fa3555488bca94
Pauline Hilborn <philbo130@gmail.com>
2016-03-26 15:12:06 -0400

    adding index pages

--- bibtucker/config/routes.rb
+++ bibtucker/config/routes.rb
@@ -1,5 +1,50 @@
 Rails.application.routes.draw do
+  
   devise_for :users
+  root 'users#index'
+  get 'secure' => 'users#secure'
+
+  resources :users
+  resources :likes
+
+#   Routes
+#                  Prefix Verb   URI Pattern                    Controller#Action
+#         new_user_session GET    /users/sign_in(.:format)       devise/sessions#new
+#             user_session POST   /users/sign_in(.:format)       devise/sessions#create
+#     destroy_user_session DELETE /users/sign_out(.:format)      devise/sessions#destroy
+#            user_password POST   /users/password(.:format)      devise/passwords#create
+#        new_user_password GET    /users/password/new(.:format)  devise/passwords#new
+#       edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
+#                          PATCH  /users/password(.:format)      devise/passwords#update
+#                          PUT    /users/password(.:format)      devise/passwords#update
+# cancel_user_registration GET    /users/cancel(.:format)        devise/registrations#cancel
+#        user_registration POST   /users(.:format)               devise/registrations#create
+#    new_user_registration GET    /users/sign_up(.:format)       devise/registrations#new
+#   edit_user_registration GET    /users/edit(.:format)          devise/registrations#edit
+#                          PATCH  /users(.:format)               devise/registrations#update
+#                          PUT    /users(.:format)               devise/registrations#update
+#                          DELETE /users(.:format)               devise/registrations#destroy
+#                     root GET    /                              users#index
+#                   secure GET    /secure(.:format)              users#secure
+#                    users GET    /users(.:format)               users#index
+#                          POST   /users(.:format)               users#create
+#                 new_user GET    /users/new(.:format)           users#new
+#                edit_user GET    /users/:id/edit(.:format)      users#edit
+#                     user GET    /users/:id(.:format)           users#show
+#                          PATCH  /users/:id(.:format)           users#update
+#                          PUT    /users/:id(.:format)           users#update
+#                          DELETE /users/:id(.:format)           users#destroy
+#                    likes GET    /likes(.:format)               likes#index
+#                          POST   /likes(.:format)               likes#create
+#                 new_like GET    /likes/new(.:format)           likes#new
+#                edit_like GET    /likes/:id/edit(.:format)      likes#edit
+#                     like GET    /likes/:id(.:format)           likes#show
+#                          PATCH  /likes/:id(.:format)           likes#update
+#                          PUT    /likes/:id(.:format)           likes#update
+#                          DELETE /likes/:id(.:format)           likes#destroy
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
